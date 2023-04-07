77bfc61fd186606c33d65a20e3ede680f5eefaf0
Daniil Ruzhkov <d.ruzhkov7@gmail.com>
2023-04-07 23:32:30 +0300

    changed productrepository: crudrepository -> jparepository

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/repositories/ProductRepository.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package ru.alishev.springcourse.FirstSecurityApp.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.alishev.springcourse.FirstSecurityApp.models.Product;
@@ -7,6 +8,6 @@ import ru.alishev.springcourse.FirstSecurityApp.models.Product;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     Optional<Product> findByName(String name);
 }
